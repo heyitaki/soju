7a334afd123e20cd7c3bfcad6b67f144e3e48fab
heyitaki <akshath@berkeley.edu>
2020-12-19 19:05:04 -0800

    parsing single champ works

--- src/parsers/parsers/spiders/champ_stats.py
+++ src/parsers/parsers/spiders/champ_stats.py
@@ -6,15 +6,40 @@ class ChampStatsSpider(scrapy.Spider):
 
   def start_requests(self):
     urls = [
-      'http://quotes.toscrape.com/page/1/',
-      'http://quotes.toscrape.com/page/2/',
+      # 'https://leagueoflegends.fandom.com/wiki/Jhin/TFT',
+      # 'https://leagueoflegends.fandom.com/wiki/Lillia/TFT',
+      'https://leagueoflegends.fandom.com/wiki/Kalista/TFT'
     ]
     for url in urls:
       yield scrapy.Request(url=url, callback=self.parse)
 
   def parse(self, response):
-    page = response.url.split('/')[-2]
-    filename = f'quotes-{page}.html'
-    with open(filename, 'wb') as f:
-      f.write(response.body)
-    self.log(f'Saved file {filename}')
+    stats = response.css('aside.pi-theme-teamfight-champion-bottom')
+
+    def get(x):
+      sel = stats.xpath(f'//div[@data-source="{x}"][1]').css('div.pi-data-value ::text').getall()
+      text = ''.join(''.join(sel).split())
+      
+      if ('/' in text):
+        return [float(i) for i in text.split('/')]
+      else:
+        try:
+          return float(text)
+        except:
+          return text
+
+    yield {
+      'name': response.url.split('/')[-2],
+      'stats': {
+        'armor': get('arm'),
+        'attack_damage': get('ad'),
+        'attack_speed': get('as'),
+        'crit_chance': .25,
+        'crit_modifier': 1.5,
+        'health': get('hp'),
+        'magic_resist': get('mr'),
+        'mana': get('mana'),
+        'mana_start': get('startmana'),
+        'range': get('range'),
+      }
+    }
