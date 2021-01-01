12f1420772f070c01e766fa6ca1e1ae09486e14e
heyitaki <akshath@berkeley.edu>
2020-12-31 16:18:14 -0800

    refactor how stats work

--- src/models/champion.py
+++ src/models/champion.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from copy import copy, deepcopy
+from types.champ_stats import StatsData
 from typing import TYPE_CHECKING, List, Union
 
 from src.load_data import load_champ_data
@@ -20,7 +21,6 @@ class Champion:
     name: str
     owner: Player
     stats: ChampStats
-    stats_base: ChampStats
     traits: List[str]
 
     def __init__(self, champ_data: ChampData):
@@ -31,16 +31,19 @@ class Champion:
         self.level = 1
         self.name = champ_data["name"]
         self.stats = ChampStats(champ_data["stats"])
-        self.stats_base = ChampStats(champ_data["stats"])
         self.traits = champ_data["traits"]
 
     def clone(self, id: int, chosen_trait: str = None, item: Item = None) -> Champion:
+        """
+        Create a copy of this champ with specified chosen trait, item, and id.
+        TODO: when implementing items + chosen, need to remove traits given by spat items
+        """
         new_champ = deepcopy(self)
         new_champ.id = id
-        new_champ.stats = copy(new_champ.stats_base)
         new_champ.level = 2 if chosen_trait else 1
         new_champ.chosen_trait = chosen_trait
         new_champ.items = [item] if item else []
+        new_champ.stats.reset()
         return new_champ
 
     def get_sell_cost(self) -> int:
@@ -51,11 +54,22 @@ class Champion:
 
 
 class ChampStats:
+    base: Stats
+    current: Stats
+
+    def __init__(self, stats: ChampStatsData):
+        self.base = Stats.from_champ_stats_data(stats)
+        self.reset()
+
+    def reset(self):
+        self.current = copy(self.base)
+
+
+class Stats:
     ability_power: float
     armor: float
     attack_damage: float
     attack_speed: float
-    base: ChampStats
     crit_chance: float
     crit_modifier: float
     dodge: float
@@ -65,25 +79,35 @@ class ChampStats:
     mana_start: int
     range: int
 
-    def __init__(self, stats: ChampStatsData = None):
-        if stats:
-            self.base = ChampStats()
-            self.base.ability_power = 1.0
-            self.base.armor = stats["armor"]
-            self.base.attack_damage = stats["attack_damage"]
-            self.base.attack_speed = stats["attack_speed"]
-            self.base.base = self.base
-            self.base.crit_chance = 0.25
-            self.base.crit_modifier = 1.5
-            self.base.dodge = 0
-            self.base.health = stats["health"]
-            self.base.magic_resist = stats["magic_resist"]
-            self.base.mana = int(stats["mana"])
-            self.base.mana_start = int(stats["mana_start"])
-            self.base.range = int(stats["range"])
+    def __init__(self, data: StatsData):
+        self.ability_power = data["ability_power"]
+        self.armor = data["armor"]
+        self.attack_damage = data["attack_damage"]
+        self.attack_speed = data["attack_speed"]
+        self.crit_chance = data["crit_chance"]
+        self.crit_modifier = data["crit_modifier"]
+        self.dodge = data["dodge"]
+        self.health = data["health"]
+        self.magic_resist = data["magic_resist"]
+        self.mana = data["mana"]
+        self.mana_start = data["mana_start"]
+        self.range = data["range"]
 
-    # def reset(self):
-
-
-cs = ChampStats(load_champ_data()[0]["stats"])
-# print([cs[k] for k in cs])
+    @classmethod
+    def from_champ_stats_data(cls, data: ChampStatsData) -> Stats:
+        return cls(
+            {
+                "ability_power": 1.0,
+                "armor": data["armor"],
+                "attack_damage": data["attack_damage"],
+                "attack_speed": data["attack_speed"],
+                "crit_chance": 0.25,
+                "crit_modifier": 1.5,
+                "dodge": 0,
+                "health": data["health"],
+                "magic_resist": data["magic_resist"],
+                "mana": int(data["mana"]),
+                "mana_start": int(data["mana_start"]),
+                "range": int(data["range"]),
+            }
+        )
--- src/types/champ_stats.py
+++ src/types/champ_stats.py
@@ -1,6 +1,21 @@
 from typing import List, TypedDict
 
 
+class StatsData(TypedDict):
+    ability_power: float
+    armor: float
+    attack_damage: float
+    attack_speed: float
+    crit_chance: float
+    crit_modifier: float
+    dodge: float
+    health: float
+    magic_resist: float
+    mana: int
+    mana_start: int
+    range: int
+
+
 class ChampStatsData(TypedDict):
     armor: float
     attack_damage: float
